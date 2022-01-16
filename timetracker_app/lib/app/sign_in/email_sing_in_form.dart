fee418c3e0da7c36af7fc477e81e9b884241db50
Ihor Debelyi <ihor.debelyi@gmail.com>
2022-01-16 22:17:20 +0100

    Improved user interaction with sign in screen

--- timetracker_app/lib/app/sign_in/email_sing_in_form.dart
+++ timetracker_app/lib/app/sign_in/email_sing_in_form.dart
@@ -1,10 +1,9 @@
-import 'dart:io';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timetracker_app/app/sign_in/validators.dart';
 import 'package:timetracker_app/common_widgets/form_submit_btn.dart';
-import 'package:timetracker_app/common_widgets/show_alert_dialog.dart';
+import 'package:timetracker_app/common_widgets/show_exception_alert_dialog.dart';
 import 'package:timetracker_app/services/auth_base.dart';
 
 enum EmailSignInFormType { signIn, register }
@@ -28,6 +27,15 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
   bool _submitted = false;
   bool _isLoading = false;
 
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
   void _submit() async {
     setState(() {
       _submitted = true;
@@ -41,12 +49,11 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
         await auth.createUserWithEmailAndPassword(_email, _password);
       }
       Navigator.of(context).pop();
-    } catch (e) {
-      showAlertDialog(
+    } on FirebaseAuthException catch (e) {
+      showExceptionAlertDialog(
         context,
         title: 'Sign in failed',
-        content: e.toString(),
-        defaultActionText: 'Ok',
+        exception: e,
       );
     } finally {
       setState(() {
--- timetracker_app/lib/app/sign_in/sign_in_page.dart
+++ timetracker_app/lib/app/sign_in/sign_in_page.dart
@@ -1,37 +1,73 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timetracker_app/app/sign_in/email_sign_in_page.dart';
+import 'package:timetracker_app/common_widgets/show_exception_alert_dialog.dart';
 import 'package:timetracker_app/services/auth_base.dart';
 import 'sign_in_btn.dart';
 import 'social_sign_in_btn.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  bool _isLoading = false;
+
+  void _updateLoadingStateTo(bool value) {
+    setState(() {
+      _isLoading = value;
+    });
+  }
+
+  void _showSignInError(BuildContext context, Exception exception) {
+    if (exception is FirebaseAuthException &&
+        exception.code == 'ERROR_ABORTED_BY_USER') {
+      return;
+    }
+    showExceptionAlertDialog(
+      context,
+      title: 'Sign in failed',
+      exception: exception,
+    );
+  }
 
   Future<void> _signInAnonymously(BuildContext context) async {
     try {
+      _updateLoadingStateTo(true);
       final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signInAnonymously();
-    } catch (e) {
-      print(e.toString());
+    } on Exception catch (e) {
+      _showSignInError(context, e);
+    } finally {
+      _updateLoadingStateTo(false);
     }
   }
 
   Future<void> _signInWithGoogle(BuildContext context) async {
     try {
+      _updateLoadingStateTo(true);
       final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signInWithGoogle();
-    } catch (e) {
-      print(e.toString());
+    } on Exception catch (e) {
+      _showSignInError(context, e);
+    } finally {
+      _updateLoadingStateTo(false);
     }
   }
 
   Future<void> _signInWithFacebook(BuildContext context) async {
     try {
+      _updateLoadingStateTo(true);
       final auth = Provider.of<AuthBase>(context, listen: false);
       await auth.signInFacebook();
-    } catch (e) {
-      print(e.toString());
+    } on Exception catch (e) {
+      _showSignInError(context, e);
+    } finally {
+      _updateLoadingStateTo(false);
     }
   }
 
@@ -64,18 +100,14 @@ class SignInPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          const Text(
-            'Sign In',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600),
-          ),
+          SizedBox(child: _buildHeader(), height: 50.0),
           const SizedBox(height: 48.0),
           SocialSignInButton(
             assetName: 'images/google-logo.png',
             text: 'Sign in with Google',
             textColor: Colors.black87,
             color: Colors.white,
-            onPressed: () => _signInWithGoogle(context),
+            onPressed: _isLoading ? null : () => _signInWithGoogle(context),
           ),
           const SizedBox(height: 8.0),
           SocialSignInButton(
@@ -83,14 +115,14 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in with Facebook',
             textColor: Colors.white,
             color: const Color(0xFF334D92),
-            onPressed: () => _signInWithFacebook(context),
+            onPressed: _isLoading ? null : () => _signInWithFacebook(context),
           ),
           const SizedBox(height: 8.0),
           SignInButton(
             text: 'Sign in with email',
             textColor: Colors.white,
             color: Colors.teal[700]!,
-            onPressed: () => _signInWithEmail(context),
+            onPressed: _isLoading ? null : () => _signInWithEmail(context),
           ),
           const SizedBox(height: 8.0),
           const Text('or',
@@ -101,10 +133,23 @@ class SignInPage extends StatelessWidget {
             text: 'Sign in anonymously',
             textColor: Colors.black87,
             color: Colors.lightGreen[300]!,
-            onPressed: () => _signInAnonymously(context),
+            onPressed: _isLoading ? null : () => _signInAnonymously(context),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildHeader() {
+    if (_isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return const Text(
+      'Sign In',
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w600),
     );
   }
 }
